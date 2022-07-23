027244ae419289cb80e6e0c86e61e360a4f95398
yong <tkddbs1644@gmail.com>
2022-07-24 03:07:19 +0900

    feat: 더미데이터 추가

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface EventData {
+  readonly id: string;
+  readonly title: string;
+  readonly description: string;
+  readonly location: string;
+  readonly date: string;
+  readonly image: string;
+  readonly isFeatured: boolean;
+}
